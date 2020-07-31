bdcec91f670638b52f50513705ea7b8c7eaf2dec
Virgil Cantu <cantu.sampsom@gmail.com>
2020-07-31 22:49:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
     else name_hash.each do |key, value|
          if min_value == nil || value < min_value
            min_value = value
-         elsif value == min_value
+         else 
            wanted_key << key
          end
        end
