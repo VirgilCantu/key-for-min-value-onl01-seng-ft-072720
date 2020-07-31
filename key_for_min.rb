f727b3da3be7abf3f4fa7048dd3008a5f1738ca9
Virgil Cantu <cantu.sampsom@gmail.com>
2020-07-31 22:02:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,13 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-
-end
+    min_value = nil
+    if name_hash == {}
+    else name_hash.each do |key, value|
+         if min_value == nil || value < min_value
+            min_value = value
+         elsif
+           name_hash[key] == min_value
+           name_hash[key]
+         end
+       end
+     end
+   end
